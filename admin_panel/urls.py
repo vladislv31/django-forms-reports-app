dbc0dd8fb988e9c8025f3301920676c7bee4c368
vladislv31 <vladislv.k31@gmail.com>
2022-03-28 13:06:48 +0300

    admin start forms page

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.views.generic.base import RedirectView
-from .views import QuestionnaireListView
+from .views import QuestionnaireListView, UserOrganizationInfoListView
 
 
 app_name = 'admin_panel'
 
 urlpatterns = [
     path('', RedirectView.as_view(url='questionnaires/'), name='index'),
+    path('start-forms/', UserOrganizationInfoListView.as_view(), name='start-forms'),
     path('questionnaires/', QuestionnaireListView.as_view(), name='questionnaires'),
 ]
--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import redirect
 from django.views.generic.list import ListView
 
-from main.models import Questionnaire
+from main.models import Questionnaire, UserOrganizationInfo
+
+
+class UserOrganizationInfoListView(ListView):
+    model = UserOrganizationInfo
+    template_name = 'admin_panel/start_forms_list.html'
+    context_object_name = 'start_forms'
 
 
 class QuestionnaireListView(ListView):
