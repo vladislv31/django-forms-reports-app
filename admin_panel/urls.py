f0bf93827f10da7490fd9ab158f123c259c675e0
vladislv31 <vladislv.k31@gmail.com>
2022-03-28 23:44:29 +0300

    parsed docs page

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.views.generic.base import RedirectView
-from .views import QuestionnaireListView, UserOrganizationInfoListView
+from .views import QuestionnaireListView, UserOrganizationInfoListView, ParsedDocumentListView
 
 
 app_name = 'admin_panel'
@@ -8,5 +8,6 @@ app_name = 'admin_panel'
 urlpatterns = [
     path('', RedirectView.as_view(url='questionnaires/'), name='index'),
     path('start-forms/', UserOrganizationInfoListView.as_view(), name='start-forms'),
+    path('parsed-documents/', ParsedDocumentListView.as_view(), name='parsed-documents'),
     path('questionnaires/', QuestionnaireListView.as_view(), name='questionnaires'),
 ]
--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import redirect
 from django.views.generic.list import ListView
 
 from main.models import Questionnaire, UserOrganizationInfo
+from .models import ParsedDocument
 
 
 class UserOrganizationInfoListView(ListView):
@@ -14,3 +14,9 @@ class QuestionnaireListView(ListView):
     model = Questionnaire
     template_name = 'admin_panel/questionnaires.html'
     context_object_name = 'questionnaire'
+
+
+class ParsedDocumentListView(ListView):
+    model = ParsedDocument
+    template_name = 'admin_panel/parsed_document_list.html'
+    context_object_name = 'parsed_documents'
